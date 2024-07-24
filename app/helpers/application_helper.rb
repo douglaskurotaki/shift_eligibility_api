e8eab9dc94d81251c45fcc305bb0eb1e3fdf63f4
Douglas Kurotaki <douglas.kurotaki.97@gmail.com>
2024-07-23 23:11:08 -0300

    style: Fix inconsistent quotation marks and brackets

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module ApplicationHelper
+  def pagination_meta(object)
+    {
+      current_page: object.current_page,
+      next_page: object.next_page,
+      prev_page: object.prev_page,
+      total_pages: object.total_pages,
+      total_count: object.total_count
+    }
+  end
+end
